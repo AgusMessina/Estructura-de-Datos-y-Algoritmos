233ca7b86f87632558139672e887c7e2e0a7959b
gusgus <73510218+AgusMessina@users.noreply.github.com>
2026-09-10 01:40:55 -0300

    Add files via upload

---
+++ 1P-Grupo36/LVO.h
@@ -0,0 +1,94 @@
+#ifndef LVO_H_INCLUDED
+#define LVO_H_INCLUDED
+
+#include "padron.h"
+#define infinito 999999999
+
+typedef struct Nodo{
+    Padron dato;
+    struct Nodo *siguiente;
+}Nodo;
+
+typedef struct{
+    Nodo *acc;
+    Nodo *cur;
+    Nodo *aux;
+    Nodo *fin;
+}Lvo;
+
+void initLVO(Lvo *lista){
+    lista->fin = (Nodo*)malloc(sizeof(Nodo));
+
+    setPadronDNI(&(lista->fin->dato), infinito);
+    lista->fin->siguiente = lista->fin; // Nodo terminacion apunta a si mismo, que es NULL (recursividad rara lol)
+
+
+    lista->acc = lista->fin;
+    lista->cur = lista->fin;
+    lista->aux = lista->fin;
+}
+
+void resetLVO(Lvo *lista){
+    lista->cur = lista->acc;
+    lista->aux = lista->acc;
+}
+
+int isEmptyLVO(Lvo lista){
+    return(lista.acc == lista.fin);
+}
+
+int isFullLvo(Lvo lista){
+    return(lista.acc != lista.cur && lista.cur == lista.fin);
+}
+
+void forwardsLvo(Lvo *lista){
+    lista->aux = lista->cur;
+    lista->cur = lista->cur->siguiente;
+}
+
+int localizarLvo(Lvo *lista, int x){
+    resetLVO(lista);
+
+    while(getPadronDNI((lista->cur->dato)) < x){     //Como la lista tiene de terminacion +infinito, nunca se pasa de largo ni chequea esta misma ("!=" Prohibido ya que si llega al lista->fin, este no seria igual)
+            forwardsLvo(lista);
+          }
+
+    if(getPadronDNI((lista->cur->dato)) == x){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int alta(Lvo *lista, Padron p){
+    //FUNCION DE COSTOOO (hay q hacer xd)
+
+    if(localizarLvo(lista, getPadronDNI(p))){
+            //EL DNI YA EXISTE
+        if(){
+            //N-TUPLA REPETIDA
+        }else{
+            //SOLO SE REPITE EL DNI
+        }
+    }else{
+        //NO EXISTE EL DNI, SE DA DE ALTA
+        Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
+        if(isFullLvo(*lista) || nuevo == NULL){
+            return 0; //NO HAY ESPACIO :(
+        }else{  //INGRESAR
+
+            nuevo->dato = p;
+
+            if(lista->acc == lista->cur){
+                nuevo->siguiente = lista->acc;
+                lista->acc = nuevo;
+            }else{
+                nuevo->siguiente = lista->cur;
+                lista->aux->siguiente = nuevo;
+            }
+            return 1; //YEY
+        }
+    }
+}
+
+#endif // LVO_H_INCLUDED
---
+++ 1P-Grupo36/padron.h
@@ -0,0 +1,56 @@
+#ifndef PADRON_H_INCLUDED
+#define PADRON_H_INCLUDED
+
+
+typedef struct{
+    int dni;
+    char nombreApellido[51];
+    char domicilio[81];
+    int codPostal;
+    int mesa;
+    int circuito;
+} Padron;
+
+void setPadronDNI(Padron *p, int x){
+    p->dni = x;
+}
+void setPadronPostal(Padron *p, int x){
+    p->codPostal = x;
+}
+void setPadronMesa(Padron *p, int x){
+    p->mesa = x;
+}
+void setPadronCircuito(Padron *p, int x){
+    p->circuito = x;
+}
+void setPadronNombreApe(Padron *p, char x []){
+    strcpy(p->nombreApellido, x);
+}
+void setPadronDomicilio(Padron *p, char x []){
+    strcpy(p->domicilio, x);
+}
+
+
+int getPadronDNI(Padron p){
+    return p.dni;
+}
+int getPadronMesa(Padron p){
+    return p.mesa;
+}
+int getPadronCodPostal(Padron p){
+    return p.codPostal;
+}
+int getPadronCircuito(Padron p){
+    return p.circuito;
+}
+char* getPadronNombreApe(Padron p){
+    char *aux = (char*)malloc(strlen(p.nombreApellido) + 1);
+    strcpy(aux, p.nombreApellido);
+    return aux;
+}
+char* getPadronNombreDomicilio(Padron p){
+    char *aux = (char*)malloc(strlen(p.domicilio) + 1);
+    strcpy(aux, p.domicilio);
+    return aux;
+}
+#endif // PADRON_H_INCLUDED
